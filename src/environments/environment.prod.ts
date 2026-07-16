3d34b4321ca7dadc57829785ba5ab57159324786
Rodrigo Rayo <rodrigorayo12@hotmail.com>
2026-07-16 10:48:29 +0200

    fix: hardcode authCallbackUrl in environments instead of using window.location.origin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  authCallbackUrl: 'https://bandyou.es/auth/callback',
   supabaseUrl: 'https://yxaurffzwtqsckfmnzdj.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4YXVyZmZ6d3Rxc2NrZm1uemRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTE2ODIsImV4cCI6MjA5MjI4NzY4Mn0.GUbfyBpaP8W_LFIT9IfMjuszgw-J87ANhOAJY8Tpj1E',
   vapidPublicKey: 'BLslfW3Qj79wALOTHJX4VV9sSDuqr1U8kjL3I4NtyB7zCats8W_qTmZNAKMD8ku44dpen2KORGtfd2fTGG3RDLs',
